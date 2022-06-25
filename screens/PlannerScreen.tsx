309059a8378f201f7a61ebfa424ca4de2cef04a1
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-06-25 15:26:06 +0200

    initial commit

---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function PlannerScreen() {
+    return (
+      <View>
+        <Text>Planner</Text>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+  });
